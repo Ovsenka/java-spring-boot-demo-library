6a2ab7bcccd5515e63f8782810e0319c30b5ab44
Ovsenka <ovsenka3@gmail.com>
2023-05-09 22:30:08 +0300

    base auth

--- src/main/java/com/ovsenka/app/controllers/MainController.java
+++ src/main/java/com/ovsenka/app/controllers/MainController.java
@@ -3,16 +3,18 @@ package com.ovsenka.app.controllers;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 
 @RestController
 @RequestMapping("/")
 public class MainController {
-    @GetMapping
-    public ModelAndView getMainPage(){
-        return new ModelAndView("mainpage");
-    }
 
     @GetMapping("/register")
     public ModelAndView getRegPage(){ return new ModelAndView("register"); }
+
+    @GetMapping("/userpage")
+    public ModelAndView getUserPage(Principal info){
+        return new ModelAndView("userpage").addObject("usrname", info.getName()); }
 
 }
