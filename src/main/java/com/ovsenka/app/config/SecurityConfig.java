f3a2a7972b60908ae6eee7b6fd5011824cb1de43
Ovsenka <ovsenka3@gmail.com>
2023-05-16 14:50:40 +0300

    JDBC auth

--- src/main/java/com/ovsenka/app/config/SecurityConfig.java
+++ src/main/java/com/ovsenka/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ovsenka.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,20 +9,38 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
     @Bean
-    InMemoryUserDetailsManager users(){
-        return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder().username("us").password("pa").roles("USER").build());
+    JdbcUserDetailsManager users(){
+        UserDetails user = User.builder()
+                .username("user1")
+                .password("{bcrypt}$2a$12$xmMz2d/EBF2QniWEorUj9uUb25QJvKFrDPfc8GOKwRBWP7OzG75NW")
+                .roles("USER")
+                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (jdbcUserDetailsManager.userExists("user1"))
+            jdbcUserDetailsManager.deleteUser("user1");
+
+        jdbcUserDetailsManager.createUser(user);
+        System.out.println("Создан новый пользователь user1!");
+        return jdbcUserDetailsManager;
     }
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated()
